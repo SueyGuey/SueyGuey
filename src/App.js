5b2b19c45515916160da78a395da07d73b93aded
Suhan Gui <suhangui6@gmail.com>
2021-04-13 14:27:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
+      </head>
+      <body>
+        <div class = "header">
+          <div class = "inside_header">
+            <div class = "name">
+              <h1>Suhan Gui</h1>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export default App;
