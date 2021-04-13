231203ce398ab73903edecea6a8060874ab0d7c9
Suhan Gui <suhangui6@gmail.com>
2021-04-13 15:41:06 -0400

    header added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
-    <html>
-      <head>
-        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
-      </head>
-      <body>
-        <div class = "header">
-          <div class = "inside_header">
-            <div class = "name">
-              <h1>Suhan Gui</h1>
+    <BrowserRouter>
+      <html>
+        <head>
+          <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
+        </head>
+        <body>
+          <div class = "header">
+            <div class = "inside_header">
+              <div class = "name">
+                <a href = "/"><h1>Suhan Gui</h1></a>
+              </div>
+              <ul class = "nav">
+                <a><li>About</li></a>
+                <a><li>Education</li></a>
+                <a><li>Portfolio</li></a>
+                <a><li>Skills</li></a>
+                <a><li>Contact</li></a>
+              </ul>
             </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+      <Switch>
+        <Route path = "/" exact component = {Home}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return (
+            <div class = "home">
+                <h1> hallo</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
