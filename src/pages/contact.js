d25fc9e4d7cf65aae967123e1f55513e4a6cdb1c
Suhan Gui <suhangui6@gmail.com>
2021-04-13 21:41:22 -0400

    added redirects

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ class Contact extends Component{
     render(){
         return (
             <div class = "default">
-                <h1 class = "title">Contact Me</h1>
+                <h1 class = "big">Contact Me</h1>
                 <h2>Email: <a href = "mailto: suhangui6@gmail.com">suhangui6@gmail.com</a></h2>
                 <h2>Phone: 603-729-7028</h2>
                 <h2>LinkedIn: <a href ="https://www.linkedin.com/in/suhan-gui-31091b1b5/">
