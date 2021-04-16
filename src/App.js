f6c7ab84ec316a3619ed2fa851213bd025b69ac1
Suhan Gui <suhangui6@gmail.com>
2021-04-16 14:48:04 -0400

    base name for browser router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Resume from './pages/resume';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <html>
         <head>
           <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
@@ -18,26 +18,26 @@ function App() {
           <div class = "header">
             <div class = "inside_header">
               <div class = "name">
-                <a href = {process.env.PUBLIC_URL + '/'}><h1>BIG CHUNGUS</h1></a>
+                <a href = '/'><h1>Suhan Gui</h1></a>
               </div>
               <ul class = "nav">
-                <a href = {process.env.PUBLIC_URL + '/resume'}><li>Resume</li></a>
-                <a href = {process.env.PUBLIC_URL + '/about'}><li>About</li></a>
-                <a href = {process.env.PUBLIC_URL + '/education'}><li>Education</li></a>
-                <a href = {process.env.PUBLIC_URL + '/portfolio'}><li>Portfolio</li></a>
-                <a href = {process.env.PUBLIC_URL + '/contact'}><li>Contact</li></a>
+                <a href = '/resume'><li>Resume</li></a>
+                <a href = '/about'><li>About</li></a>
+                <a href = '/education'><li>Education</li></a>
+                <a href = '/portfolio'><li>Portfolio</li></a>
+                <a href = '/contact'><li>Contact</li></a>
               </ul>
             </div>
           </div>
         </body>
       </html>
       <Switch>
-        <Route path = {process.env.PUBLIC_URL + '/'} exact component = {Home}/>
-        <Route path = {process.env.PUBLIC_URL + '/resume'} exact component = {Resume}/>
-        <Route path = {process.env.PUBLIC_URL + '/about'} exact component = {About}/>
-        <Route path = {process.env.PUBLIC_URL + '/education'} exact component = {Education}/>
-        <Route path = {process.env.PUBLIC_URL + '/portfolio'} exact component = {Portfolio}/>
-        <Route path = {process.env.PUBLIC_URL + '/contact'} exact component = {Contact}/>
+        <Route path = '/' exact component = {Home}/>
+        <Route path = '/resume' exact component = {Resume}/>
+        <Route path = '/about' exact component = {About}/>
+        <Route path = '/education' exact component = {Education}/>
+        <Route path = '/portfolio' exact component = {Portfolio}/>
+        <Route path = '/contact' exact component = {Contact}/>
       </Switch>
     </BrowserRouter>
   );
