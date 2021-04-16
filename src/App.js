8cc296b825b97364b037593027808de1ccf8fc6c
Suhan Gui <suhangui6@gmail.com>
2021-04-16 15:10:44 -0400

    replace a href with link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import Home from './pages/home';
 import Contact from './pages/contact';
 import Portfolio from './pages/portfolio';
@@ -18,14 +18,14 @@ function App() {
           <div class = "header">
             <div class = "inside_header">
               <div class = "name">
-                <a href = '/'><h1>Suhan Gui</h1></a>
+                <Link to = '/'><h1>Suhan Gui</h1></Link>
               </div>
               <ul class = "nav">
-                <a href = '/resume'><li>Resume</li></a>
-                <a href = '/about'><li>About</li></a>
-                <a href = '/education'><li>Education</li></a>
-                <a href = '/portfolio'><li>Portfolio</li></a>
-                <a href = '/contact'><li>Contact</li></a>
+                <Link to = '/resume'><li>Resume</li></Link>
+                <Link to = '/about'><li>About</li></Link>
+                <Link to = '/education'><li>Education</li></Link>
+                <Link to = '/portfolio'><li>Portfolio</li></Link>
+                <Link to = '/contact'><li>Contact</li></Link>
               </ul>
             </div>
           </div>
